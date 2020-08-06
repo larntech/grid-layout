975fef91d878f3c3bf61a6dd04ea9043862479a4
rkamere20 <richardkamere20@gmail.com>
2020-08-06 13:04:55 +0300

    Improved missing items

--- app/src/main/java/net/larntech/gridlayout/MainActivity.java
+++ app/src/main/java/net/larntech/gridlayout/MainActivity.java
@@ -1,16 +1,60 @@
 package net.larntech.gridlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView addUsers, users, settings, reports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addUsers = findViewById(R.id.addusers);
+        users = findViewById(R.id.users);
+        reports = findViewById(R.id.reports);
+        settings = findViewById(R.id.settings);
+
+        addUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AddUsersActivity.class));
+            }
+        });
+
+
+        users.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,UsersActivity.class));
+
+            }
+        });
+
+
+        reports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ReportsActivity.class));
+
+            }
+        });
+
+
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SettingsActivity.class));
+
+            }
+        });
+
     }
 
 
